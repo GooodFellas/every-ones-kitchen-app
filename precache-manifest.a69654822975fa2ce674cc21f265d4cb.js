3aebe8f3d9861575b4c7f3013c510502b981662a
Sheetal Ramisetti <sheethalramisetti@gmail.com>
2020-08-06 14:10:57 -0500

    Updates

---
+++ precache-manifest.a69654822975fa2ce674cc21f265d4cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90af6734e54437d71606ba4945109f3",
+    "url": "/every-ones-kitchen-app/index.html"
+  },
+  {
+    "revision": "3127c7430d51bcd803d1",
+    "url": "/every-ones-kitchen-app/static/css/2.309d8041.chunk.css"
+  },
+  {
+    "revision": "fa1e7dce82102ba5ac4e",
+    "url": "/every-ones-kitchen-app/static/css/main.ecbce28f.chunk.css"
+  },
+  {
+    "revision": "3127c7430d51bcd803d1",
+    "url": "/every-ones-kitchen-app/static/js/2.3f609a77.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/every-ones-kitchen-app/static/js/2.3f609a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1e7dce82102ba5ac4e",
+    "url": "/every-ones-kitchen-app/static/js/main.eda0930b.chunk.js"
+  },
+  {
+    "revision": "157d3e74906ea7e2fac0",
+    "url": "/every-ones-kitchen-app/static/js/runtime-main.759c7e36.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/every-ones-kitchen-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/every-ones-kitchen-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/every-ones-kitchen-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/every-ones-kitchen-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
